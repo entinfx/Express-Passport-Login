f621f26217a7d17250c040f8b5f8d4f1993b4899
entinfx <monkacry@gmail.com>
2022-09-20 16:48:58 +0500

    Cleanup, install dependencies.

--- app.js
+++ app.js
@@ -17,45 +17,22 @@ app.use(express.urlencoded({ extended: true }))
 let users = []
 
 /* User authentication routes */
-// Index page: /
+// Index page: GET /
 app.get('/', (req, res) => {
     res.render('./index', { title: 'Auth Demo', body: 'Passport Auth Demo!' })
 })
 
-// Login form: /login
-app.get('/login', (req, res) => {
-    res.render('./login', { title: 'Log In' })
-})
-
-app.post('/login', (req, res) => {
-    const { scryptSync, timingSafeEqual } = require('crypto')
-
-    const email = req.body.email
-    const providedPassword = req.body.password
-    const user = users.find(u => u.email === email )
-
-    if (!user) {
-        console.log(`User not found`)
-        return
-    }
-
-    const [storedSalt, storedPasswordHash] = user.passwordHash.split(':')
-    const providedPasswordHash = scryptSync(providedPassword, storedSalt, 64)
-    const match = timingSafeEqual(providedPasswordHash, Buffer.from(storedPasswordHash, 'hex'))
-
-    if (match) {
-        res.render('./index', { title: 'Auth Demo', body: 'Welcome back!' })
-    } else {
-        console.log('Incorrect login data!')
-        res.render('./login', { title: 'Log In | Error' })
-    }
-})
-
-// Sign Up form: /signup
+// Sign Up form: GET /signup
 app.get('/signup', (req, res) => {
     res.render('./signup', { title: 'Sign Up' })
 })
 
+// Login form: GET /login
+app.get('/login', (req, res) => {
+    res.render('./login', { title: 'Log In' })
+})
+
+// Sign Up request: POST /signup
 app.post('/signup', (req, res) => {
     const { scryptSync, randomBytes } = require('crypto')
 
@@ -74,4 +51,29 @@ app.post('/signup', (req, res) => {
     })
 
     res.render('./index', { title: 'Auth Demo', body: 'Hello, Welcome!' })
+})
+
+// Log In request: POST /login
+app.post('/login', (req, res) => {
+    const { scryptSync, timingSafeEqual } = require('crypto')
+
+    const email = req.body.email
+    const providedPassword = req.body.password
+
+    const user = users.find(u => u.email === email )
+
+    if (!user) {
+        console.log(`User not found`)
+        return
+    }
+
+    const [storedSalt, storedPasswordHash] = user.passwordHash.split(':')
+    const providedPasswordHash = scryptSync(providedPassword, storedSalt, 64)
+    const match = timingSafeEqual(providedPasswordHash, Buffer.from(storedPasswordHash, 'hex'))
+
+    if (match) {
+        res.render('./index', { title: 'Auth Demo', body: 'Welcome back!' })
+    } else {
+        res.render('./login', { title: 'Log In | Error' })
+    }
 })
